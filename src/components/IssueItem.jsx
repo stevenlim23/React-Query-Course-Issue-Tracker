6aae3f138bf902fffddea13af7b779a1d6172b3e
Steven Salim <steven.salim@grip-principle.com>
2023-07-27 16:41:07 +0700

    add image and helper for user data

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 
 // Helper
 import { relativeDate } from "../helpers/relativeDate";
+import { useUserData } from "../helpers/useUserData";
 
 // Icon
 import { GoIssueOpened, GoIssueClosed, GoComment } from "react-icons/go";
@@ -16,6 +17,9 @@ export const IssueItem = ({
   labels,
   status,
 }) => {
+  const assigneeUser = useUserData(assignee);
+  const createdByUser = useUserData(createdBy);
+
   return (
     <li>
       <div>
@@ -36,11 +40,20 @@ export const IssueItem = ({
           ))}
         </span>
         <small>
-          #{number} opened {relativeDate(createdDate)} by {createdBy}
+          #{number} opened {relativeDate(createdDate)}
+          {createdByUser.isSuccess && `by ${createdByUser.data.name}`}
         </small>
       </div>
 
-      {assignee && <div>{assignee}</div>}
+      {assignee && (
+        <img
+          className="assigned-to"
+          alt={`Assigned To ${
+            assigneeUser.isSuccess ? assigneeUser.data.name : "Avatar"
+          }`}
+          src={assigneeUser.isSuccess && assigneeUser.data.profilePictureUrl}
+        />
+      )}
 
       <span className="comment-count">
         {commentCount > 0 && (
